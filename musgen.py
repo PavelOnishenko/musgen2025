14b6d8865eac0e26772632402d3e54f13cd08ef6
Cliff <biznezmail2016@gmail.com>
2025-10-03 23:53:18 +0200

    Style.

--- musgen.py
+++ musgen.py
@@ -5,12 +5,12 @@ mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
 
-channel = 9  # 10-й канал для ударных
+channel = 9 
 KICK, SNARE, HIHAT = 36, 38, 42
 
 ticks_per_beat = mid.ticks_per_beat
-note_len = ticks_per_beat // 8   # длительность = 1/32
-eighth = ticks_per_beat // 2     # для позиционирования хэта
+note_len = ticks_per_beat // 8
+eighth = ticks_per_beat // 2
 
 for bar in range(4):
     for beat in range(4):
@@ -32,16 +32,13 @@ for bar in range(4):
             track.append(Message('note_off', note=SNARE, velocity=0, time=0, channel=channel))
             
 
-        # пауза = оставшееся время до середины доли
         rest = eighth - note_len
         if rest > 0:
             track.append(Message('note_on', note=HIHAT, velocity=0, time=rest, channel=channel))  # пустышка
 
-        # второй удар (середина доли)
         track.append(Message('note_on', note=HIHAT, velocity=60, time=0, channel=channel))
         track.append(Message('note_off', note=HIHAT, velocity=0, time=note_len, channel=channel))
 
-        # пауза = оставшееся время до конца доли
         rest = eighth - note_len
         if rest > 0:
             track.append(Message('note_on', note=HIHAT, velocity=0, time=rest, channel=channel))  # пустышка
